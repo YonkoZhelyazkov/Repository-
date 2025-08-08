c0442fec12e445844623dd30a65c4e29167ccdac
YonkoZhelyazkov <yoj9777@gmail.com>
2025-08-08 14:03:17 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="text-8xl flex justify-center ">
+          This is our awesome intern project
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="instagram.com/"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/instagram.svg"
+            alt="File icon"
+            width={24}
+            height={24}
+          />
+          Instagram
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/YonkoZhelyazkov/Repository-"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/github-outline.svg"
+            alt="Window icon"
+            width={20}
+            height={20}
+          />
+          Github
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="/support"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Support
+        </a>
+      </footer>
+    </div>
+  );
+}
+
