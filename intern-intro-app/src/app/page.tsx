5a09c1ee8a8232caf2a648603329c4ee494e15c5
YonkoZhelyazkov <yoj9777@gmail.com>
2025-08-13 14:37:05 +0300

    The footer is now where it should be and now using Link components for in app navigation. The footer includes links to Instagram, GitHub, and a support page. The code has been updated to use Next.js's Link component for internal navigation, ensuring proper client-side routing.

--- intern-intro-app/src/app/page.tsx
+++ intern-intro-app/src/app/page.tsx
@@ -8,53 +8,6 @@ export default function Home() {
           This is our awesome intern project
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <Image
-            aria-hidden
-            src="/instagram.svg"
-            alt="File icon"
-            width={24}
-            height={24}
-          />
-          Instagram
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/YonkoZhelyazkov/Repository-"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/github-outline.svg"
-            alt="Window icon"
-            width={20}
-            height={20}
-          />
-          Github
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="/support"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Support
-        </a>
-      </footer>
     </div>
   );
 }
